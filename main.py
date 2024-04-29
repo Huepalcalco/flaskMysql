c0a490c21beebdb21eacfa1167edd51fb40b75e0
Isaac F <alienxtrix@gmail.com>
2024-04-28 19:56:29 -0600

    Crud products complete

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, make_response, redirect, render_template, url_for
+from flask import Flask, request, make_response, redirect, render_template, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask (__name__)
 
+#mysql connection
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '1234'
 app.config['MYSQL_DB'] = 'Escuela'
 mysql=MySQL(app) 
+
+#setting
+app.secret_key = 'mysecretkey'
 
 todos = ['Comprar','Solicitud', 'entrega']
 
@@ -22,7 +26,10 @@ def index():
     response = make_response(redirect('/hello'))
     response.set_cookie('user_ip', user_ip)
 
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM Products')
+    data = cur.fetchall()
+    return render_template('index.html', Products=data)
 
 @app.route('/hello')
 def hello():
@@ -37,24 +44,45 @@ def hello():
 @app.route('/add_contact', methods=['POST'])
 def add_contact():
     if request.method=="POST":
-        fullname= request.form['fullname']
+        product= request.form['product']
         phone= request.form['phone']
         email= request.form['email']
-        print(fullname)
+        print(product)
         cur=mysql.connection.cursor()
-        cur.execute("INSERT INTO Products (`name`) VALUES ('"+str(fullname)+"');")
+        cur.execute("INSERT INTO Products (`name`) VALUES ('"+str(product)+"');")
+        flash('Producto Added successfully')
         # mycursor.execute("INSERT INTO `gish_listening_db`.`Media` (`name`, `id_mediaType`, `id_MediaStatus`) VALUES ('"+str(name)+"', '"+str(mediaType)+"', '1');")
         mysql.connection.commit()
         
         return redirect(url_for('index'))
         
-    
 
-@app.route('/edit_contact')
-def edit_contact():
-    return "editar contacto"
+@app.route('/edit/<id>')
+def get_Product(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM Products WHERE id ='+(id))
+    data = cur.fetchall()
+    return render_template('edit-product.html', product = data[0])
 
-@app.route('/delete_contact')
-def delete_contact():
-    return "delete contacto"
+@app.route('/update/<id>', methods= ['POST'])
+def update_Product(id):
+    if request.method == 'POST':
+        product= request.form['product']
+        phone= request.form['phone']
+        email= request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute("UPDATE Products SET name='"+str(product)+"' WHERE id ="+(id))
+        # (product, phone, email, id))   
+        mysql.connection.commit()
+        flash('producto actualizado')
+        return redirect(url_for('index'))
 
+@app.route('/delete/<string:id>')
+def delete_Product(id):
+    cur = mysql.connection.cursor()
+    cur.execute(' DELETE  FROM Products WHERE id = {0}'.format(id))
+    mysql.connection.commit()
+    flash('Producto eliminado')
+    return redirect(url_for('index'))
+
+ 
