9ace27438e374d3bcadf49d3cb11e48fcb77ffc4
Huepalcalco <jacquuelinehuepalcalco@gmail.com>
2024-05-01 23:48:03 -0600

    update 4

--- main.py
+++ main.py
@@ -95,14 +95,14 @@ def getAutoria():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM autoria')
     data = cur.fetchall()
-    return render_template('Autoria.html', Autoria=data)
+    return render_template('autoria.html', Autoria=data)
 
 @app.route('/edit/<id>')
 def get_Atoria(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM autoria WHERE id ='+(id))
     data = cur.fetchall()
-    return render_template('edit-Autoria.html', autoria = data[0])
+    return render_template('edit-autoria.html', autoria = data[0])
 
 
 @app.route('/cliente')
@@ -110,14 +110,14 @@ def getCliente():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM cliente')
     data = cur.fetchall()
-    return render_template('Cliente.html', Cliente=data)
+    return render_template('cliente.html', Clientes=data)
 
 @app.route('/edit/<id>')
 def get_Cliente(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM cliente WHERE id ='+(id))
     data = cur.fetchall()
-    return render_template('edit-Cliente.html', cliente = data[0])
+    return render_template('edit-cliente.html', cliente = data[0])
 
 
 @app.route('/credencial')
@@ -125,14 +125,14 @@ def getCredencial():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM credencial')
     data = cur.fetchall()
-    return render_template('Credencial.html', Credencial=data)
+    return render_template('credencial.html', Credencial=data)
 
 @app.route('/edit/<id>')
 def get_Credencial(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM credencial WHERE id ='+(id))
     data = cur.fetchall()
-    return render_template('edit-Credencial.html', credencial = data[0])
+    return render_template('edit-credencial.html', credencial = data[0])
 
 
 @app.route('/d_prestamo')
@@ -140,14 +140,14 @@ def getD_prestamo():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM detalles_prestamo')
     data = cur.fetchall()
-    return render_template('D_prestamo.html', D_prestamo=data)
+    return render_template('d_prestamo.html', D_prestamo=data)
 
 @app.route('/edit/<id>')
 def get_D_presamo(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM detalles_prestamo WHERE id ='+(id))
     data = cur.fetchall()
-    return render_template('edit-D_prestamos.html', D_prestamos = data[0])
+    return render_template('edit-d_prestamos.html', D_prestamos = data[0])
 
 
 @app.route('/direccion')
@@ -155,14 +155,14 @@ def getDireccion():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM direccion')
     data = cur.fetchall()
-    return render_template('Direccion.html', Direccion=data)
+    return render_template('direccion.html', Direccion=data)
 
 @app.route('/edit/<id>')
 def get_Direccion(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM direccion WHERE id ='+(id))
     data = cur.fetchall()
-    return render_template('edit-Direccion.html', direccion = data[0])
+    return render_template('edit-direccion.html', direccion = data[0])
 
 
 @app.route('/editorial')
@@ -170,14 +170,14 @@ def getEditoail():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM editorial')
     data = cur.fetchall()
-    return render_template('Editorial.html', Editorial=data)
+    return render_template('editorial.html', Editorial=data)
 
 @app.route('/edit/<id>')
 def get_Editorial(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM editorial WHERE id ='+(id))
     data = cur.fetchall()
-    return render_template('edit-Editorial.html', editorial = data[0])
+    return render_template('edit-editorial.html', editorial = data[0])
 
 
 @app.route('/empleado')
@@ -185,14 +185,14 @@ def getEmpleado():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM empleado')
     data = cur.fetchall()
-    return render_template('Empleado.html', Empleado=data)
+    return render_template('empleado.html', Empleado=data)
 
 @app.route('/edit/<id>')
 def get_Empleado(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM empleado WHERE id ='+(id))
     data = cur.fetchall()
-    return render_template('edit-Empleado.html', empleado = data[0])
+    return render_template('edit-empleado.html', empleado = data[0])
 
 
 @app.route('/escolaridad')
@@ -200,14 +200,14 @@ def getEscolaridad():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM escolaridad')
     data = cur.fetchall()
-    return render_template('Escolaridad.html', Escolaridad=data)
+    return render_template('escolaridad.html', Escolaridad=data)
 
 @app.route('/edit/<id>')
 def get_Escolaridad(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM escolaridad WHERE id ='+(id))
     data = cur.fetchall()
-    return render_template('edit-Escolaridad.html', escolaridad = data[0])
+    return render_template('edit-escolaridad.html', escolaridad = data[0])
 
 
 @app.route('/estado')
@@ -215,14 +215,14 @@ def getEstado():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM estado')
     data = cur.fetchall()
-    return render_template('Estado.html', Estado=data)
+    return render_template('estado.html', Estado=data)
 
 @app.route('/edit/<id>')
 def get_Estado(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM estado WHERE id ='+(id))
     data = cur.fetchall()
-    return render_template('edit-Estado.html', estado = data[0])
+    return render_template('edit-estado.html', estado = data[0])
 
 
 @app.route('/genero')
@@ -230,14 +230,14 @@ def getGenero():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM genero')
     data = cur.fetchall()
-    return render_template('Genero.html', Genero=data)
+    return render_template('genero.html', Genero=data)
 
 @app.route('/edit/<id>')
 def get_Genero(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM genero WHERE id ='+(id))
     data = cur.fetchall()
-    return render_template('edit-Genero.html', genero = data[0])
+    return render_template('edit-genero.html', genero = data[0])
 
 
 @app.route('/idioma')
@@ -245,14 +245,14 @@ def getIdioma():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM idiona')
     data = cur.fetchall()
-    return render_template('Idioma.html', Idioma=data)
+    return render_template('idioma.html', Idioma=data)
 
 @app.route('/edit/<id>')
 def get_Idioma(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM idioma WHERE id ='+(id))
     data = cur.fetchall()
-    return render_template('edit-Idioma.html', idioma = data[0])
+    return render_template('edit-idioma.html', idioma = data[0])
 
 
 @app.route('/libro')
@@ -260,28 +260,28 @@ def getLibro():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM libro')
     data = cur.fetchall()
-    return render_template('Libro.html', Libro=data)
+    return render_template('libro.html', Libro=data)
 
 @app.route('/edit/<id>')
 def get_Libro(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM libro WHERE id ='+(id))
     data = cur.fetchall()
-    return render_template('edit-Libro.html', libro = data[0])
+    return render_template('edit-libro.html', libro = data[0])
 
 @app.route('/pais')
 def getPais():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM pais')
     data = cur.fetchall()
-    return render_template('Pais.html', Pais=data)
+    return render_template('pais.html', Pais=data)
 
 @app.route('/edit/<id>')
 def get_Pais(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM pais WHERE id ='+(id))
     data = cur.fetchall()
-    return render_template('edit-Pais.html', pais = data[0])
+    return render_template('edit-pais.html', pais = data[0])
 
 
 @app.route('/prestamo')
@@ -289,14 +289,14 @@ def getPrestamo():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM prestamo')
     data = cur.fetchall()
-    return render_template('Prestamo.html', Prestamo=data)
+    return render_template('prestamo.html', Prestamo=data)
 
 @app.route('/edit/<id>')
 def get_Prestamo(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM prestamo WHERE id ='+(id))
     data = cur.fetchall()
-    return render_template('edit-Prestamo.html', prestamo = data[0])
+    return render_template('edit-prestamo.html', prestamo = data[0])
 
 
 @app.route('/sucursal')
@@ -304,14 +304,14 @@ def getSucursal():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM sucursal')
     data = cur.fetchall()
-    return render_template('Sucursal.html', Sucursal=data)
+    return render_template('sucursal.html', Sucursal=data)
 
 @app.route('/edit/<id>')
 def get_Sucursal(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM sucursal WHERE id ='+(id))
     data = cur.fetchall()
-    return render_template('edit-Sucursal.html', sucursal = data[0])
+    return render_template('edit-sucursal.html', sucursal = data[0])
 
 
 
